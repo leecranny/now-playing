4e9ce6750d7748d8a12fd10ae9c68acf63fb1445
Christian Delprete <hello@bitprism.co>
2023-07-20 16:34:04 -0500

    Fixed promise.

--- src/spotify/SpotifyAPI.js
+++ src/spotify/SpotifyAPI.js
@@ -3,5 +3,6 @@ const NOW_PLAYING_ENDPOINT = process.env.REACT_APP_API_ENDPOINT;
 
 export default async function getNowPlayingItem(
 ) {
-  return fetch(NOW_PLAYING_ENDPOINT);
+  const response = await fetch(NOW_PLAYING_ENDPOINT);
+  return response.json();
 }
